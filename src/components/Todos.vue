e53691dbb9c78dfed5aa8448297f3a74958bf394
MohamedJH <mohamedjahha0@gmail.com>
2021-10-27 05:00:23 +0100

    commit adddTodo component

---
+++ src/components/Todos.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <h3>Todos</h3>
+    <div class="legend">
+      <span>Double click to mark as complete</span>
+      <span> <span class="incomplete-box"></span> = Incomplete </span>
+      <span> <span class="complete-box"></span> = Complete </span>
+    </div>
+  </div>
+  <div class="todos">
+    <div v-for="todo in allTodos" :key="todo.id" class="todo">
+      {{ todo.title }}
+    </div>
+    <i @click="deleteTodo(todo.id)" class="fas fa-trash-alt"></i>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+export default {
+  name: "Todos",
+  methods: {
+    ...mapActions(["fetchTodos"]),
+  },
+  computed: mapGetters(["allTodos"]),
+  created() {
+    this.fetchTodos();
+  },
+};
+</script>
+
+<style scoped>
+.todos {
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: repeat(auto-fit, 20rem);
+}
+.todo {
+  border: 2px solid #c790cc;
+  border-radius: 20px;
+  background: #6c2056;
+  color: #c790cc;
+  font-weight: 500;
+  padding: 1rem;
+  border-radius: 5px;
+  text-align: center;
+  position: relative;
+  cursor: pointer;
+}
+
+i {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  color: #fff;
+  cursor: pointer;
+}
+.legend {
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 1rem;
+}
+.complete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #35495e;
+}
+.incomplete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #41b883;
+}
+.is-complete {
+  background: #35495e;
+  color: #fff;
+}
+@media (max-width: 500px) {
+  .todos {
+    grid-template-columns: 1fr;
+  }
+}
+</style>
---
+++ src/store/modules/todos.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+const state = {
+  todos: [],
+};
+
+const getters = {
+  allTodos: (state) => state.todos,
+};
+
+const actions = {
+  async fetchTodos({ commit }) {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/todos"
+    );
+
+    commit("setTodos", response.data);
+  },
+  async addTodo({ commit }, title) {
+    const response = await axios.post(
+      "https://jsonplaceholder.typicode.com/todos",
+      { title, completed: false }
+    );
+    console.log(response.data);
+    commit("addTodo", response.data);
+  },
+};
+
+const mutations = {
+  setTodos: (state, listTodos) => (state.todos = listTodos),
+  addTodo: (state, newTodos) => (state.todos = [newTodos, ...state.todos]),
+};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations,
+};
